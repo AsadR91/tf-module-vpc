0f88744e3ce3209854bb32c98f20c59fc9f45fc6
arafique458 <arafique458@gmail.com>
2025-01-20 20:32:07 -0500

    VPC and Subnet Setup

--- main.tf
+++ main.tf
@@ -1,6 +1,44 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.cidr_block
-  enable_dns_support = true
+  cidr_block           = var.cidr_block
+  enable_dns_support   = true
   enable_dns_hostnames = true
-  tags = merge(var.tags, { Name = "${var.env}-vpc" })
+  tags                 = merge(var.tags, { Name = "${var.env}-vpc" })
 }
+
+module "subnets" {
+  source = "./subnets"
+
+  for_each   = var.subnets
+  vpc_id     = aws_vpc.main.id
+  cidr_block = each.value["cidr_block"]
+  name       = each.value["name"]
+  azs        = each.value["azs"]
+
+  tags = var.tags
+  env  = var.env
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(var.tags, { Name = "${var.env}-igw" })
+}
+
+resource "aws_eip" "ngw" {
+  count = length(lookup(lookup(var.subnets, "public", null), "cidr_block", 0))
+  vpc   = true
+  tags  = merge(var.tags, { Name = "${var.env}-ngw" })
+}
+
+//resource "aws_nat_gateway" "ngw" {
+//  allocation_id = aws_eip.example.id
+//  subnet_id     = aws_subnet.example.id
+//
+//  tags = {
+//    Name = "gw NAT"
+//  }
+//
+//  # To ensure proper ordering, it is recommended to add an explicit dependency
+//  # on the Internet Gateway for the VPC.
+//  depends_on = [aws_internet_gateway.example]
+//}
