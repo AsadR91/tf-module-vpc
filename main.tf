eded432763cf17847bbb7b7e8abd25a78cad1410
arafique458 <arafique458@gmail.com>
2025-01-22 21:48:44 -0500

    Added the TF-Module-app and Tf-module-vpc

--- main.tf
+++ main.tf
@@ -26,7 +26,6 @@ resource "aws_internet_gateway" "igw" {
 
 resource "aws_eip" "ngw" {
   count = length(var.subnets["public"].cidr_block)
-  vpc   = true
   tags  = merge(var.tags, { Name = "${var.env}-ngw" })
 }
 
@@ -39,15 +38,34 @@ resource "aws_nat_gateway" "ngw" {
 }
 
 resource "aws_route" "igw" {
-  count = length(module.subnets["public"].route_table_ids)
-  route_table_id = module.subnets["public"].route_table_ids[count.index]
-  gateway_id = aws_internet_gateway.igw.id
+  count                  = length(module.subnets["public"].route_table_ids)
+  route_table_id         = module.subnets["public"].route_table_ids[count.index]
+  gateway_id             = aws_internet_gateway.igw.id
   destination_cidr_block = "0.0.0.0/0"
 }
 
-# resource "aws_route" "ngw" {
-#   count = length(local.all_private_subnet_ids)
-#   route_table_id = local.all_private_subnet_ids[count.index]
-#   nat_gateway_id = aws_nat_gateway.ngw.id
-#   destination_cidr_block = "0.0.0.0/0"
-# }
+resource "aws_route" "ngw" {
+  count                  = length(local.all_private_subnet_ids)
+  route_table_id         = local.all_private_subnet_ids[count.index]
+  nat_gateway_id         = element(aws_nat_gateway.ngw.*.id, count.index)
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_vpc_peering_connection" "peer" {
+  peer_vpc_id = var.default_vpc_id
+  vpc_id      = aws_vpc.main.id
+  auto_accept = true
+}
+
+resource "aws_route" "peering-connection-route" {
+  count                     = length(local.all_private_subnet_ids)
+  route_table_id            = element(local.all_private_subnet_ids, count.index)
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  destination_cidr_block    = var.default_vpc_cidr
+}
+
+resource "aws_route" "peering-connection-route-in-default-vpc" {
+  route_table_id            = var.default_vpc_rtid
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  destination_cidr_block    = var.cidr_block
+}
